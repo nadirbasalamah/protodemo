d35efd17c32904ffd68f51bc7f3e5f69dbcf5894
nadirbasalamah <nadir008basalamah@gmail.com>
2021-08-05 10:23:32 +0700

    feat(main): Adding convert to JSON function. Menambahkan fungsi konversi ke JSON dari proto message.

--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/proto"
+	student "github.com/nadirbasalamah/protodemo/src/model"
+)
 
 func main() {
-	fmt.Println("test")
+	// create a student entity
+	var newStudent student.Student = student.Student{
+		Name:      "Nathan Mckane",
+		StudentId: "RVN2021",
+		CardNumber: &student.Student_IdCardNumber{
+			IdCardNumber: "12345321",
+		},
+	}
+
+	// add some courses
+	newStudent.Courses = []*student.Course{
+		{
+			Code: "C001",
+			Name: "Algorithm",
+		},
+		{
+			Code: "C002",
+			Name: "Data Structure",
+		},
+	}
+
+	// print courses
+	fmt.Println("Courses")
+	for _, v := range newStudent.Courses {
+		fmt.Println(v)
+	}
+
+	// convert to JSON
+	var result string = convertToJSON(&newStudent)
+	fmt.Println("to JSON: ", result)
+
+}
+
+func convertToJSON(pb proto.Message) string {
+	var marshaler jsonpb.Marshaler = jsonpb.Marshaler{}
+	result, err := marshaler.MarshalToString(pb)
+
+	if err != nil {
+		log.Fatalln("Cant convert to JSON", err)
+		return ""
+	}
+	return result
 }
